f530f4fbe667dbc950e18f365fb66589288c9b41
Aliaksei Belski <aliaksei_belski@epicgames.com>
2021-10-18 22:32:32 -0700

    Add ls for debug purpose

--- gatsby-node.js
+++ gatsby-node.js
@@ -160,5 +160,5 @@ exports.onPreBuild = async (gatsbyNodeHelpers) => {
     if (stdout) reporter.info(stdout);
   };
 
-  reportOut(await exec("yarn netlify-build"));
+  reportOut(await exec("ls && yarn netlify-build"));
 };
