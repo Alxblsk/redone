4394c8293e58d0a227bfa141f3425da60c2a51b9
Aliaksei B <mr.belski@gmail.com>
2023-04-29 13:15:52 -0700

    rearrangements

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,11 +9,6 @@ class Template extends React.Component {
   render() {
     const { children, location } = this.props;
 
-    let rootPath = `/`;
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`;
-    }
-
     return (
       <div className="wrapper">
         <PageHeader location={location} />
