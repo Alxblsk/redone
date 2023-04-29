daef5ffb7db18f6b64014cfeeb5c2b5207c175c2
Aliaksei B <mr.belski@gmail.com>
2023-04-29 13:06:27 -0700

    tracking and downgrade

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,13 +64,29 @@ module.exports = {
       options: contentfulConfig,
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["G-N09W5H37GM"],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "G-N09W5H37GM",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
       },
-    },
-    {
-      resolve: `gatsby-plugin-minify-classnames`,
     },
     {
       resolve: `gatsby-transformer-remark`,
